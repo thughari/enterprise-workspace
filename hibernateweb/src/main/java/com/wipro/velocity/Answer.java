065b38f84b4c3e08a99d52c2545ca8ac699f90b8
Hari <haribabutatikonda3@gmail.com>
2022-06-28 11:02:10 +0530

    main

---
+++ hibernateweb/src/main/java/com/wipro/velocity/Answer.java
@@ -0,0 +1,37 @@
+package com.wipro.velocity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="answers")
+public class Answer {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.TABLE)
+    private int id;   
+    private String answername;   
+    private String postedBy;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAnswername() {
+		return answername;
+	}
+	public void setAnswername(String answername) {
+		this.answername = answername;
+	}
+	public String getPostedBy() {
+		return postedBy;
+	}
+	public void setPostedBy(String postedBy) {
+		this.postedBy = postedBy;
+	}
+	   
+}
---
+++ springweb-mvc/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body bgcolor="aqua">
+	<h2 align="center">Spring Web MVC Application</h2>
+	<hr color="red">
+	<br>
+	<br>
+	<!-- "raj" is a mapping name of a method in Controller -->
+	<a href="raj">Hello World</a>
+	<!-- httpRequest to Controller -->
+</body>
+</html>
